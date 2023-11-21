c3c0eec1c60a6bf7b9682704ccb1a408e9ff7476
Jane Cho <janechodance@gmail.com>
2023-11-21 11:19:56 -0500

    add background and logo

--- practice-2/App.tsx
+++ practice-2/App.tsx
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useDeviceOrientation } from "@react-native-community/hooks";
+import { StatusBar } from "expo-status-bar";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  Image,
+  Button,
+  Dimensions,
+  ImageBackground,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        source={require("./assets/backGround.png")}
+        resizeMode="cover"
+        style={{
+          flex: 1,
+        }}
+      >
+        <Image
+          source={require("./assets/logo.png")}
+          resizeMode="contain"
+          style={{ width: 100, height: 100, alignSelf: "center", flex: 0.2 }}
+        />
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
